009b1d9aeb72c754b8400ed0763d509dfbe09d53
RuslanYarosh <ruslan.yarosh@bk.ru>
2016-08-19 16:07:40 +0300

    Update Task.rb

--- Task.rb
+++ Task.rb
@@ -1,18 +1,18 @@
 # Task-1
 def candies (x,y,z,w)
-  if x,y,z,w <=0
-    puts  "Enter the number > 0"
-else
- n1=w/x
-n2=w/y
-n3=w/z
-summ=[]
-summ=(1..n1)*x + (1..n1)*y +(1..n1)*z
- summ.each do |a| 
-if a = w
-result =+1
-else
-print "result=0"
+    if x,y,z,w <=0
+       puts  "Enter the number > 0"
+    else
+       n1=w/x
+       n2=w/y
+       n3=w/z
+       summ=[]
+       summ=(1..n1)*x + (1..n1)*y +(1..n1)*z
+      summ.each do |a| 
+          if a = w
+          result =+1
+          else
+          print "result=0"
 end
 end
 
