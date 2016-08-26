ff0374ef7b0b1f9e1e4c8791e08a0589366416c4
Rusik <ruslan.yarosh@bk.ru>
2016-08-26 15:18:39 +0300

    Finish

--- Task-2.rb
+++ Task-2.rb
@@ -9,15 +9,13 @@ def jenifer(x,y,n)
   else
       rus=[x,y].sort  # sorting an array
       k=rus[1].to_f/rus[0] # determine how much slower the second printer
-  xerocs_copy2=(n/(k+1)).to_i # the number of photocopies which will make slow xerox
-  xerocs_copy1=(n-xerocs_copy2)
-  xerox2=xerocs_copy2*rus[1]+rus[0]
-  xerox1=xerocs_copy1*rus[0]
-  puts xerox2 #time xerox 2
-  puts xerox1 #time xerox 1
-  time=[xerox1,xerox2].sort
-  result_time=time[1]
-  puts result_time #full time to copy
+      xerocs_copy2=(n/(k+1)).to_i # the number of photocopies which will make slow Xerox
+      xerocs_copy1=(n-xerocs_copy2) # the number of photocopies that will make faster Xerox
+      xerox2=xerocs_copy2*rus[1]+rus[0]  # time of slow Xerox
+      xerox1=xerocs_copy1*rus[0]  # time of faster Xerox
+      time=[xerox1,xerox2].sort  # sort values in the array
+      result_time=time[1]  # take the highest value
+      puts result_time #full time to copy
   end
 end     
 
